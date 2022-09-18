9cced9b551618699f5dc204433323d1c59843c72
nazzcelik <nazclk1903@gmail.com>
2022-09-19 01:09:31 +0300

    C# Derslerim

---
+++ GENERİC/Musteri.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    internal class Musteri
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string mail { get; set; }
+
+        public Musteri(int id, string isim, string soyisim, string mail)
+        {
+            this.id = id;
+            this.isim = isim;   
+            this.soyisim = soyisim; 
+            this.mail = mail;
+        }
+        public override string ToString()
+        {
+            return "ID: " + this.id + " / " + "İsim: " + this.isim + " / " + "Soyisim: " + this.soyisim + " / " + "Mail Adresi: " + this.mail;
+        }
+
+
+
+    }
+}
---
+++ GENERİC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Musteri> musteriler = new List<Musteri>();
+            musteriler.Add(new Musteri(123123, "Naz", "ÇELİK", "naz.celik@gmail.com"));
+            musteriler.Add(new Musteri(124124, "Asel", "ÇELİK", "asel.celik@gmail.com"));
+            musteriler.Add(new Musteri(125125, "Arjin", "ÇELİK", "arjin.celik@gmail.com"));
+
+            foreach (Musteri M1 in musteriler)
+            {
+                Console.WriteLine(M1);
+                Console.WriteLine("------------------------------------------------------------------------------");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
